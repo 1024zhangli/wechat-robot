bba23e19e97d82ce9f628dbe3790857843cc12b9
lingchen <lingchen@migu.cn>
2018-06-11 22:33:44 +0800

    调整调度时间为非正点，调整了需要发送的内容

--- index.js
+++ index.js
@@ -1,15 +1,40 @@
 const wechat = require('./src/wechat')
 const job = require('./src/jobs')
-
+const formatDate = (nS, Ft) => {
+    Date.prototype.format = function (fmt) {
+        var o = {
+            'M+': this.getMonth() + 1, // 月份
+            'd+': this.getDate(), // 日
+            'h+': this.getHours(), // 小时
+            'm+': this.getMinutes(), // 分
+            's+': this.getSeconds(), // 秒
+            'q+': Math.floor((this.getMonth() + 3) / 3), // 季度
+            'S': this.getMilliseconds() // 毫秒
+        };
+        if (/(y+)/.test(fmt)) {
+            fmt = fmt.replace(RegExp.$1, (this.getFullYear() + '').substr(4 - RegExp.$1.length));
+        }
+        for (var k in o) {
+            if (new RegExp('(' + k + ')').test(fmt)) {
+                fmt = fmt.replace(RegExp.$1, (RegExp.$1.length === 1) ? (o[k]) : (('00' + o[k]).substr(('' + o[k]).length)));
+            }
+        }
+        return fmt;
+    }
+    Ft = Ft || 'yyyy-MM-dd hh:mm:ss';
+    return new Date(parseInt(nS)).format(Ft);
+}
 const main = async () => {
     const user = await wechat.loginToWeChat()
     if (user) {
         console.log(`欢迎欢迎，${user.name()}登入成功！`)
         // 此处改为你需要发消息的群名
-        const room = await wechat.queryRoom('巴啦啦测试群')
-        // 22:00 - 06:00的每个小时的整点会执行
+        const room = await wechat.queryRoom('咪咕前端闲聊群')
+        // 执行时间见jobs.js
+        const currentDate = formatDate(new Date().getTime(), 'yyyy-MM-dd');
+        const currentHour = new Date().getHours();
         job.scheduleJob(() => {
-            room.say(`现在时间是${new Date().toLocaleString()}\n我很想你`)
+            room.say(`时间：${currentDate}（${currentHour-1}:00-${currentHour}:00）\n结论：H5页面正常`);
         })
     }
 }
